61a4478e950a173ac3d54d81d3ebd9498e28142f
revgato <locchoihay147@gmail.com>
2023-01-26 00:17:55 +0700

    global variable

--- communicate.h
+++ communicate.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "game.h"
 
-#define SERVER_PORT 5500
+#define SERVER_PORT 5504
 #define BUFF_SIZE 1024
 #define BACKLOG 30
 #define SERVER_ADDR "127.0.0.1"
@@ -53,6 +53,7 @@ void copy_waiting_room_type(waiting_room_type *dest, waiting_room_type src);
 void copy_game_state_type(game_state_type *dest, game_state_type src);
 client_room_type init_client_room();
 void send_all(client_room_type client_room, conn_msg_type conn_msg);
+client_room_type copy_client_room(client_room_type client_room);
 
 // Define function's body
 
@@ -120,6 +121,7 @@ conn_msg_type make_conn_msg(conn_msg_type_type type, conn_data_type data)
 
 client_room_type init_client_room()
 {
+    // client_room_type client_room;
     client_room_type client_room;
     for (int i = 0; i < client_room.joined; i++)
     {
@@ -142,6 +144,20 @@ void send_all(client_room_type client_room, conn_msg_type conn_msg)
             exit(1);
         }
     }
+}
+
+client_room_type copy_client_room(client_room_type client_room)
+{
+    printf("Client room joined in function: %d\n", client_room.joined);
+    client_room_type new_client_room;
+    for (int i = 0; i < PLAYER_PER_ROOM; i++)
+    {
+        new_client_room.connfd[i] = client_room.connfd[i];
+        strcpy(new_client_room.username[i], client_room.username[i]);
+        new_client_room.status[i] = client_room.status[i];
+    }
+    new_client_room.joined = client_room.joined;
+    return new_client_room;
 }
 
 #endif
